7580278ae19e25be61db3145dfb1e0e081faab96
Mhdagsf <mhdagsf20@gmail.com>
2025-12-27 21:23:58 +0700

    templating setup

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use App\Http\Controllers\Admin\DashboardController;
+use App\Http\Controllers\CheckoutController;
+use App\Http\Controllers\DetailController;
 use App\Http\Controllers\HomeController;
 use Illuminate\Support\Facades\Route;
 
@@ -16,6 +18,9 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', [HomeController::class, 'index'])->name('home');
+Route::get('/detail', [DetailController::class, 'index'])->name('detail');
+Route::get('/checkout', [CheckoutController::class, 'index'])->name('checkout');
+Route::get('/checkout/success', [CheckoutController::class, 'success'])->name('checkout-success');
 
 Route::prefix('admin')
     ->name('admin.')
