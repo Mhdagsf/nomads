7e6b6b5cf799f886da2ad0f84d12070eed9fe31c
Mhdagsf <mhdagsf20@gmail.com>
2026-01-15 01:03:21 +0700

    update travel-package table admin

--- app/Http/Controllers/Auth/VerificationController.php
+++ app/Http/Controllers/Auth/VerificationController.php
@@ -25,7 +25,7 @@ class VerificationController extends Controller
      *
      * @var string
      */
-    protected $redirectTo = '/hoe';
+    protected $redirectTo = '/';
 
     /**
      * Create a new controller instance.
